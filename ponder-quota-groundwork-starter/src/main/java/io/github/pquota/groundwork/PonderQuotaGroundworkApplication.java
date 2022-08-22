cfdd5fa06da4c803f7951d4843e04adf48f553f5
0027025249 <yao.zepeng@iwhalecloud.com>
2022-08-22 16:26:03 +0800

    repository initialization

---
+++ ponder-quota-groundwork-starter/src/main/java/io/github/pquota/groundwork/PonderQuotaGroundworkApplication.java
@@ -0,0 +1,13 @@
+package io.github.pquota.groundwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonderQuotaGroundworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonderQuotaGroundworkApplication.class, args);
+    }
+
+}
